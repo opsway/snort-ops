14145878306828dd99496e4f89e9781e004a8057
Andriy Samilyak <samilyak@gmail.com>
2013-07-02 14:04:17 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "ktylxy@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures snort-ops"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
 depends "snort"
